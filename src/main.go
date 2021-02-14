aca5a9623a77a7dc6f90ae808d341da6a106e651
Reece Mercer <reecemercer981@gmail.com>
2021-02-14 02:36:50 +0000

    Update timer & top 4 boxes

--- src/main.go
+++ src/main.go
@@ -14,6 +14,7 @@ type PiCLIData struct {
 	Settings            *Settings
 	FormattedAPIAddress string
 	APIKey              string
+	LastUpdated         time.Time
 }
 
 var piCLIData = PiCLIData{}
@@ -47,15 +48,24 @@ func startUI() {
 	domainsOnBlocklist.Title = "Domains on Blocklist"
 	domainsOnBlocklist.SetRect(75, 0, 100, 3)
 
+	lastUpdated := widgets.NewParagraph()
+	lastUpdated.SetRect(0, 3, 25, 6)
+	lastUpdated.Border = false
+
 	draw := func() {
 		basicData.update()
 		totalQueries.Text = fmt.Sprintf("%d", basicData.QueriesToday)
 		queriesBlocked.Text = fmt.Sprintf("%d", basicData.BlockedToday)
-		ui.Render(totalQueries, queriesBlocked, percentBlocked, domainsOnBlocklist)
+		percentBlocked.Text = fmt.Sprintf("%f", basicData.PercentBlockedToday)
+		domainsOnBlocklist.Text = fmt.Sprintf("%d", basicData.DomainsOnBlocklist)
+		lastUpdated.Text = fmt.Sprintf("Last updated: %s", piCLIData.LastUpdated.Format("15:04:05"))
+		ui.Render(totalQueries, queriesBlocked, percentBlocked, domainsOnBlocklist, lastUpdated)
 	}
 
 	uiEvents := ui.PollEvents()
-	ticker := time.NewTicker(time.Second / 10).C
+	tickerDuration := time.Duration(piCLIData.Settings.RefreshS)
+	ticker := time.NewTicker(time.Second * tickerDuration).C
+	draw()
 	for {
 		select {
 		case e := <-uiEvents:
@@ -64,6 +74,7 @@ func startUI() {
 				return
 			}
 		case <-ticker:
+			piCLIData.LastUpdated = time.Now()
 			draw()
 		}
 	}
